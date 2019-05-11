4d81b00ffbfffb937ec817155926e970620bbda6
Ethan Sagin <ethansagin@gmail.com>
2019-05-11 00:35:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,8 +20,10 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |k, v|
+      self.send(("#{k}="), v)
     binding.pry
     end
+    self
   end
 
   def self.all
