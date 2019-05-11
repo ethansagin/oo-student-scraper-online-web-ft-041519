2c419788eec0ebd9117cacedc64fefdad9f2798c
Ethan Sagin <ethansagin@gmail.com>
2019-05-11 00:26:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |k, v|
+      self.send("#{}")
   end
 
   def self.create_from_collection(students_array)
